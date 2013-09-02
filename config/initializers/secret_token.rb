9881d278b91f4a56d258a07b833c32cbba4ce5d8
Matthew Star <your.mattjstar@gmail.com>
2013-09-02 14:11:56 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GmapsTest::Application.config.secret_key_base = '4ff6a40a08e189944a4fd809ef8eb59440ffd9b0dccad11ecfcb0b2641de27b96383dbcde349af6ef1f8fa29c688204e96b4d3547838dd3602a0456d285eed23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GmapsTest::Application.config.session_store :cookie_store, key: '_gmaps_test_session'
